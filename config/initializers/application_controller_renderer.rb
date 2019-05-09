cdf2d60e7fdb64994471240f5ecee3d65621ac34
kyotarooooo <i.kentaro-0916@outlook.com>
2019-05-09 11:24:29 +0900

    [finish]Railsを学ぼう

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '4672f2f0f5c2ad9a878472a5d24bd12c3134f2cc2f27d098c6d5acaf00d41431771721be58bc0e514fba2c3f35ff69e7e5579170bc3940653cdc040b5a98f078'
